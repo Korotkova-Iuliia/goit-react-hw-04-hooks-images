e3417c81a6bd160692113ea08321404d98070c8a
Korotkova-Iuliia <3589900@gmail.com>
2022-02-03 22:28:34 +0200

    searchBar

--- src/components/App.js
+++ src/components/App.js
@@ -117,3 +117,110 @@ export class App extends Component {
     );
   }
 }
+// export class App extends Component {
+//   state = {
+//     listImages: [],
+//     searchImage: '',
+//     page: 1,
+//     isLoading: false,
+//     largeImageURL: null,
+//     showModal: false,
+//     hasMore: true,
+//   };
+//   componentDidUpdate(_, prevState) {
+//     const { listImages, searchImage, page } = this.state;
+//     if (prevState.searchImage !== searchImage && listImages.length > 0) {
+//       this.setState({ listImages: [], page: 1 });
+//     }
+
+//     if (prevState.searchImage !== searchImage || prevState.page !== page) {
+//       this.getAxioslistImages();
+//     }
+//     if (prevState.listImages.length > 0 && listImages.length > 0) {
+//       this.scrollBy();
+//     }
+//   }
+//   scrollBy = () => {
+//     window.scrollBy({
+//       top: 600,
+//       behavior: 'smooth',
+//     });
+//   };
+//   getAxioslistImages = async () => {
+//     const { searchImage, page } = this.state;
+//     this.setState({ isLoading: true });
+//     try {
+//       const data = await getAxiosTag(searchImage, page);
+//       this.setState(prevState => ({
+//         listImages: [...prevState.listImages, ...data.hits],
+//         isLoading: false,
+//       }));
+//       if (data.totalHits === 0) {
+//         toast.warn('Cannot find your request!');
+//       }
+//       if (page > data.totalHits / 12 && data.totalHits !== 0) {
+//         toast.warn('You reach end of search!');
+//         this.setState({ hasMore: false });
+//       }
+//       if (data.totalHits !== 0 && page === 1) {
+//         toast.warn(`Hoooray! We search ${data.totalHits} images `);
+//       }
+//       return;
+//     } catch (error) {
+//       toast.warn('sorry, try again later');
+//     }
+//   };
+//   handleLoadMore = () => {
+//     this.setState(prevState => ({
+//       page: prevState.page + 1,
+//     }));
+//   };
+//   handleSearchBar = imageName => {
+//     this.setState({ searchImage: imageName });
+//   };
+//   selectImages = (largeImageURL, tags, showModal) => {
+//     this.setState({ largeImageURL, tags, showModal: !showModal });
+//   };
+//   closeModal = () => {
+//     this.setState({ showModal: false });
+//   };
+//   render() {
+//     const { isLoading, listImages, largeImageURL, tags, showModal, hasMore } =
+//       this.state;
+//     return (
+//       <>
+//         <GlobalStyle />
+//         <Container>
+//           <Searchbar onSearch={this.handleSearchBar} />
+//           {isLoading && (
+//             <SpinnerWrepper>
+//               <Oval
+//                 ariaLabel="isLoading-indicator"
+//                 height={70}
+//                 width={70}
+//                 strokeWidth={4}
+//                 color="gray"
+//                 secondaryColor="yellow"
+//               />
+//             </SpinnerWrepper>
+//           )}
+//           {listImages.length > 0 && (
+//             <ImageGallery
+//               listImages={listImages}
+//               onSelectImages={this.selectImages}
+//             />
+//           )}
+//           {hasMore && listImages.length > 0 && (
+//             <Button type="button" loadMore={this.handleLoadMore} />
+//           )}
+//           {showModal && (
+//             <Modal onClose={this.closeModal}>
+//               <img src={largeImageURL} alt={tags} />
+//             </Modal>
+//           )}
+//           <ToastContainer position="top-center" autoClose={2000} />
+//         </Container>
+//       </>
+//     );
+//   }
+// }
--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,56 +1,97 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import PropTypes from 'prop-types';
 import { toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import Icon from '../icon/Icon';
 import { SurchBarWrapper } from './Searchbar.styled';
-import { SearchForm } from './Searchbar.styled';
-import { SearchFormButton } from './Searchbar.styled';
-import { SearchFormButtonLabel } from './Searchbar.styled';
-import { SearchFormInput } from './Searchbar.styled';
-class Searchbar extends Component {
-  state = {
-    searchImage: '',
+import {
+  SearchForm,
+  SearchFormButton,
+  SearchFormButtonLabel,
+  SearchFormInput,
+} from './Searchbar.styled';
+function Searchbar({ onSearch }) {
+  const [searchImage, setSearchImage] = useState('');
+
+  const handleInputName = e => {
+    setSearchImage(e.currentTarget.value.toLowerCase());
   };
-  handleInputName = e => {
-    this.setState({
-      searchImage: e.currentTarget.value,
-    });
-  };
-  handleSubmit = e => {
+  const handleSubmit = e => {
     e.preventDefault();
-    if (this.state.searchImage.trim() === '') {
+    if (searchImage.trim() === '') {
       return toast.warn('поисковая строка пуста!');
     }
-    this.props.onSearch(this.state.searchImage);
-    this.setState({
-      searchImage: '',
-    });
+    onSearch(searchImage);
+    setSearchImage('');
   };
-  render() {
-    const { searchImage } = this.state;
-    return (
-      <SurchBarWrapper>
-        <SearchForm onSubmit={this.handleSubmit}>
-          <SearchFormButton type="submit">
-            <SearchFormButtonLabel></SearchFormButtonLabel>
-            <Icon name="search" fill="gray" width="30px" height="30px" />
-          </SearchFormButton>
-          <SearchFormInput
-            type="text"
-            autoComplete="off"
-            value={searchImage}
-            autoFocus
-            onChange={this.handleInputName}
-            placeholder="Search images and photos"
-          />
-        </SearchForm>
-      </SurchBarWrapper>
-    );
-  }
+
+  return (
+    <SurchBarWrapper>
+      <SearchForm onSubmit={handleSubmit}>
+        <SearchFormButton type="submit">
+          <SearchFormButtonLabel></SearchFormButtonLabel>
+          <Icon name="search" fill="gray" width="30px" height="30px" />
+        </SearchFormButton>
+        <SearchFormInput
+          type="text"
+          autoComplete="off"
+          value={searchImage}
+          autoFocus
+          onChange={handleInputName}
+          placeholder="Search images and photos"
+        />
+      </SearchForm>
+    </SurchBarWrapper>
+  );
 }
 export default Searchbar;
 Searchbar.propTypes = {
   onSubmit: PropTypes.func,
   onChange: PropTypes.func,
 };
+// class Searchbar extends Component {
+//   state = {
+//     searchImage: '',
+//   };
+//   handleInputName = e => {
+//     this.setState({
+//       searchImage: e.currentTarget.value,
+//     });
+//   };
+//   handleSubmit = e => {
+//     e.preventDefault();
+//     if (this.state.searchImage.trim() === '') {
+//       return toast.warn('поисковая строка пуста!');
+//     }
+//     this.props.onSearch(this.state.searchImage);
+//     this.setState({
+//       searchImage: '',
+//     });
+//   };
+//   render() {
+//     const { searchImage } = this.state;
+//     return (
+//       <SurchBarWrapper>
+//         <SearchForm onSubmit={this.handleSubmit}>
+//           <SearchFormButton type="submit">
+//             <SearchFormButtonLabel></SearchFormButtonLabel>
+//             <Icon name="search" fill="gray" width="30px" height="30px" />
+//           </SearchFormButton>
+//           <SearchFormInput
+//             type="text"
+//             autoComplete="off"
+//             value={searchImage}
+//             autoFocus
+//             onChange={this.handleInputName}
+//             placeholder="Search images and photos"
+//           />
+//         </SearchForm>
+//       </SurchBarWrapper>
+//     );
+//   }
+// }
+// export default Searchbar;
+// Searchbar.propTypes = {
+//   onSubmit: PropTypes.func,
+//   onChange: PropTypes.func,
+// };
