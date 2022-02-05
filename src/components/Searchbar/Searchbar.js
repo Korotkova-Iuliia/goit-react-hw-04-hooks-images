bc079dd0fe43d7344dc82c95639219e286380973
Korotkova-Iuliia <3589900@gmail.com>
2022-02-05 12:44:52 +0200

    clean

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -12,7 +12,6 @@ import {
 } from './Searchbar.styled';
 function Searchbar({ onSearch }) {
   const [searchImage, setSearchImage] = useState('');
-
   const handleInputName = e => {
     setSearchImage(e.currentTarget.value.toLowerCase());
   };
@@ -24,7 +23,6 @@ function Searchbar({ onSearch }) {
     onSearch(searchImage);
     setSearchImage('');
   };
-
   return (
     <SurchBarWrapper>
       <SearchForm onSubmit={handleSubmit}>
@@ -49,49 +47,3 @@ Searchbar.propTypes = {
   onSubmit: PropTypes.func,
   onChange: PropTypes.func,
 };
-// class Searchbar extends Component {
-//   state = {
-//     searchImage: '',
-//   };
-//   handleInputName = e => {
-//     this.setState({
-//       searchImage: e.currentTarget.value,
-//     });
-//   };
-//   handleSubmit = e => {
-//     e.preventDefault();
-//     if (this.state.searchImage.trim() === '') {
-//       return toast.warn('поисковая строка пуста!');
-//     }
-//     this.props.onSearch(this.state.searchImage);
-//     this.setState({
-//       searchImage: '',
-//     });
-//   };
-//   render() {
-//     const { searchImage } = this.state;
-//     return (
-//       <SurchBarWrapper>
-//         <SearchForm onSubmit={this.handleSubmit}>
-//           <SearchFormButton type="submit">
-//             <SearchFormButtonLabel></SearchFormButtonLabel>
-//             <Icon name="search" fill="gray" width="30px" height="30px" />
-//           </SearchFormButton>
-//           <SearchFormInput
-//             type="text"
-//             autoComplete="off"
-//             value={searchImage}
-//             autoFocus
-//             onChange={this.handleInputName}
-//             placeholder="Search images and photos"
-//           />
-//         </SearchForm>
-//       </SurchBarWrapper>
-//     );
-//   }
-// }
-// export default Searchbar;
-// Searchbar.propTypes = {
-//   onSubmit: PropTypes.func,
-//   onChange: PropTypes.func,
-// };
